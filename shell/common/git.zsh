3879155193e6fa09a742513c957c2ab3b7d437aa
Jeff Diteodoro <jeff.diteodoro@klaviyo.com>
2026-04-20 09:45:57 -0400

    Add _prr_parse_url helper for prr launcher

--- shell/common/git.zsh
+++ shell/common/git.zsh
@@ -14,6 +14,7 @@ alias gpb="git symbolic-ref --short HEAD"
 alias gpbc="git symbolic-ref --short HEAD | pbcopy"
 alias gmu='git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@") && git pull'
 alias rml="rm .git/index.lock"
+alias ghm="gh pr merge --squash --delete-branch"
 
 # Git functions
 function gcp() {
@@ -33,4 +34,13 @@ function gpr() {
 function gpx() {
     git checkout '*'"$1"'*'
     git status
+}
+
+_prr_parse_url() {
+    local input="$1"
+    if [[ "$input" =~ ^https://github\.com/([^/]+)/([^/]+)/pull/([0-9]+)([/?#].*)?$ ]]; then
+        echo "${match[1]} ${match[2]} ${match[3]}"
+        return 0
+    fi
+    return 1
 }
